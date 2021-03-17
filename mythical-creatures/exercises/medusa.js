3710ad0e0efe7cbcf1247e915bec83d0f55f1a41
Travis <mckinstrytg1@gmail.com>
2021-03-17 16:18:55 -0600

    Pass medusa tests

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,24 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(victim) {
+    var newVictim = new Statue(victim.name);
+    if(this.statues.length < 3) {
+      this.statues.push(newVictim);
+    }
+    else {
+      var freedPerson = new Person(this.statues[0].name);
+      this.statues.shift();
+      this.statues.push(newVictim);
+      freedPerson.mood = 'relieved';
+      return freedPerson;
+    }
+  }
+}
+
+module.exports = Medusa;
