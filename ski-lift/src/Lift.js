99404550178b8a284312987bcd3106b0a73336a1
Travis <mckinstrytg1@gmail.com>
2021-03-24 11:32:11 -0600

    Passes Lift tests, adds Pluralize to yarn.lock file

--- ski-lift/src/Lift.js
+++ ski-lift/src/Lift.js
@@ -1,7 +1,31 @@
+var pluralize = require('pluralize');
 var Skier = require('./Skier');
 
 class Lift {
-  
+  constructor(limit) {
+    this.inService = true;
+    this.limit = limit;
+    this.skiers = [];
+    this.safetyBar = 'up';
+  }
+  admitSkier(name, hasTicket) {
+    if(this.skiers.length < this.limit && hasTicket == true) {
+      var person = new Skier(name, hasTicket);
+      this.skiers.push(person);
+    } else if(hasTicket == true){
+      return `Sorry, ${name}. Please wait for the next lift!`;
+    } else {
+      return `Sorry, ${name}. You need a lift ticket!`;
+    }
+  }
+  startLift() {
+    if(this.skiers.length == this.limit) {
+      this.safetyBar = 'down';
+    } else {
+      var amount = this.limit - this.skiers.length;
+      return `We still need ${amount} more ${pluralize('skier', amount)}!`;
+    }
+  }
 }
 
 module.exports = Lift;
