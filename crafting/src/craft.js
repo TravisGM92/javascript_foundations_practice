aa2b31de36a264bab7bcd931ab17c23b5e3bcea2
Travis <mckinstrytg1@gmail.com>
2021-03-24 11:09:41 -0600

    Passes Craft tests

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,20 @@
 class Craft {
-
+  constructor(data) {
+    this.name = data['type'];
+    this.materials = data['materials'];
+    this.completed = false;
+  }
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!';
+  }
+  calculateProjectTotal() {
+    var total = 0;
+    this.materials.forEach(item => {
+      total += (item.price * item.amount);
+    })
+    return total;
+  }
 }
 
 module.exports = Craft;
