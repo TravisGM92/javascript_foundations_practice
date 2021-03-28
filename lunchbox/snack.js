48c70f1d7da85f4b94232b1604384fac7607796c
Travis <mckinstrytg1@gmail.com>
2021-03-28 14:31:12 -0600

    Passes all Snack tests

--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,24 @@
 class Snack {
-
+  constructor(type) {
+    this.type = type;
+    this.deliciousLevel = 'extra';
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+  }
+  getEaten() {
+    this.amount -= 10;
+    if(this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  }
+  checkForHealthy() {
+    if(this.type.includes('Fruit') || this.type.includes('fruit')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 module.exports = Snack;
