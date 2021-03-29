2a6ba0175f0b5d76c5d7ee302935acdad8480132
Travis <mckinstrytg1@gmail.com>
2021-03-29 15:18:14 -0600

    Passes Gear tests

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,15 @@
 class Gear {
-
+  constructor(type, quantity) {
+    this.type = type;
+    this.quantity = quantity || 0;
+  }
+  checkForValidity() {
+    if(this.type == 'ammunition' || this.type == 'food' || this.type == 'clothes') {
+      return `Great, we'll need lots of ${this.type}!`
+    } else {
+      return `I don't think a ${this.type} will help us.`
+    }
+  }
 }
 
 module.exports = Gear;
--- oregon-trail/test/gear-test.js
+++ oregon-trail/test/gear-test.js
@@ -2,7 +2,7 @@ var assert = require('chai').assert;
 var Gear = require('../src/gear.js');
 
 describe('Gear', function() {
-  it.skip('should have a type', function() {
+  it('should have a type', function() {
     var food = new Gear('food');
     var ammunition = new Gear('ammunition');
 
@@ -10,7 +10,7 @@ describe('Gear', function() {
     assert.equal(ammunition.type, 'ammunition');
   });
 
-  it.skip('should be able to have a quantity', function () {
+  it('should be able to have a quantity', function () {
     var food = new Gear('food', 10);
     var clothes = new Gear('clothes', 12);
 
@@ -19,7 +19,7 @@ describe('Gear', function() {
   });
 
   // valid types are food, ammunition, and clothes
-  it.skip('should not be able to have an invalid type', function() {
+  it('should not be able to have an invalid type', function() {
     var ammunition = new Gear('ammunition');
     var food = new Gear('food');
     var clothes = new Gear('clothes');
@@ -29,7 +29,7 @@ describe('Gear', function() {
     var invalid = invalid.checkForValidity();
 
     assert.equal(valid, 'Great, we\'ll need lots of ammunition!');
-    assert.equal(invalid, 'I don\`t think a mp3 player will help us.');
+    assert.equal(invalid, 'I don\'t think a mp3 player will help us.');
 
     assert.equal(ammunition.type, 'ammunition');
     assert.equal(food.type, 'food');
