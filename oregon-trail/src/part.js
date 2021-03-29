663978a265b9550ea8673c25142a103ada5a950e
Travis <mckinstrytg1@gmail.com>
2021-03-29 15:21:02 -0600

    Passes all Part tests

--- oregon-trail/src/part.js
+++ oregon-trail/src/part.js
@@ -1,5 +1,20 @@
 class Part {
-
+  constructor(type) {
+    this.type = type;
+    this.broken = false;
+    this.breakNumber = 0;
+  }
+  break() {
+    this.breakNumber += 1;
+    this.broken = true;
+  }
+  repair() {
+    if(this.breakNumber < 2) {
+      this.broken = false;
+    } else {
+      return `This ${this.type} has seen better days, we'll need a brand new one!`;
+    }
+  }
 }
 
 module.exports = Part;
