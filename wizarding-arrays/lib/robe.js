89b307dbfa76b0c9fd6f748fe135a48e626af84e
Travis <mckinstrytg1@gmail.com>
2021-03-31 17:23:47 -0600

    Passes all Robe class tests

--- wizarding-arrays/lib/robe.js
+++ wizarding-arrays/lib/robe.js
@@ -3,11 +3,27 @@ class Robe {
     this.pockets = [[], [], [], [], [], [], [], [], [], []];
   }
   addToPocket(item) {
-    for (let i = 0; i < this.pockets.length; i++) {
+    for (let i = 0; i <= this.pockets.length; i++) {
       if(this.pockets[i].length == 0) {
         this.pockets[i].push(item);
         break;
       }
+    }
+  }
+  retrieve(item) {
+    var result = false;
+    for (let i = 0; i < this.pockets.length; i++ ) {
+      if(this.pockets[i].includes(item)) {
+        result = true;
+        var itemRetrieved = item;
+        this.pockets[i] = [];
+        return itemRetrieved;
+      }
+    }
+  }
+  emptyAllPockets() {
+    for (let i = 0; i < this.pockets.length; i++) {
+      this.pockets[i] = [];
     }
   }
 }
